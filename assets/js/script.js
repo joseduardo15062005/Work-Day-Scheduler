be3706d416c7782e698523c0c6cbf1909730fe3d
joseduardo15062005 <joseduardo15062005@gmail.com>
2021-08-28 17:44:31 -0700

    add bootstrap input-group for each hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 $(".time-block").on("click", "button", function () {
+  
   var hour = $(this).attr("data-hour");
   console.log(hour);
-  const descriptionEl = "#text" + hour;
+  const descriptionEl = "#description" + hour;
   const description = $(descriptionEl).val();
   console.log("description:", description);
 });
